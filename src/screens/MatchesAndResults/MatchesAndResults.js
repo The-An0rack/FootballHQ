c39b97ea9ed53c7d9043e6e18fedc09d73455bbb
TheAn0rack <dewashish3590@yahoo.com>
2024-07-04 15:03:44 +0530

    initial commit

---
+++ src/screens/MatchesAndResults/MatchesAndResults.js
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native";
+
+function MatchesAndResults() {
+    return (
+        <View>
+            <Text>MatchesAndResults Screen</Text>
+        </View>
+    );
+}
+
+export default MatchesAndResults;
